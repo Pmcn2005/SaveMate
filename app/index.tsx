cd4411d39c922ec58617ed0d2f0ebd93fec028f5
pedro <pmcn2005@gmail.com>
2025-01-29 12:29:17 +0000

    Add StatusBar color

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { View, Text } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
 import { Link } from 'expo-router'
 import React from 'react'
 
@@ -7,6 +8,8 @@ const Index = () => {
         <View className="flex-1 items-center justify-center ">
             <Text className="text-3xl font-pblack">SaveMate</Text>
             <Link href="/home" >Go to home</Link>
+
+            <StatusBar backgroundColor='#161622' style='light' />
         </View>
     )
 }
