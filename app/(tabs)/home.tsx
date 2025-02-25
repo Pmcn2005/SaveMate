3c5b754867435adfdd8c13cd02029767c67e4f48
Pedro Nunes <pedro.c.nunes@tecnico.ulisboa.pt>
2025-02-25 17:28:48 +0000

    Refactor Home and Settings components to use SafeAreaView and improve layout

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,11 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Home = () => {
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView className='bg-primary'>
+            <View className='flex my-6 px-4 space-y-6'>
+
+                <Text className='font-pblack'>HOME</Text>
+            </View> 
+        </SafeAreaView>
     )
 }
 
--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,11 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Settings = () => {
     return (
-        <View>
-            <Text>Settings</Text>
-        </View>
+        <SafeAreaView className='flex-1 bg-primary'>
+            <View className='items-center justify-center'>
+                <Text className='font-pblack bg-grey '>SETTINGS</Text>
+            </View>
+            <View className='items-center justify-center'>
+                <Text className='bg-secondary'>SETTINGS2</Text>
+            </View>
+        </SafeAreaView>
     )
 }
 
